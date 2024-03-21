2974ea2ada6d301905a4a75ed53229aae127028f
tarekichalalen2002 <t_ichalalen@estin.dz>
2024-03-21 23:07:20 +0100

    correcting the gitignor

--- main/schedulemanager/admin.py
+++ main/schedulemanager/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Enseignant
+from .models import Institution
 
 # Register your models here.
 admin.site.register(Enseignant)
+admin.site.register(Institution)
