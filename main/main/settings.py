c948dbf4bb9b1a713cb8d391336e4d25c1dd0fd7
Nadir <n_naityahia@estin.dz>
2024-03-21 21:03:38 +0100

    add requirements

--- main/main/settings.py
+++ main/main/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'schedulemanager.apps.SchedulemanagerConfig',
+    'schedulemanager.apps.SchedulemanagerConfi g',
 
 ]
 
