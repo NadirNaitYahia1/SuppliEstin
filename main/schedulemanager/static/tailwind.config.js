7e00e41bc09f8bb662e4bc0de44016ca3f324830
Nadir <n_naityahia@estin.dz>
2024-03-24 13:33:55 +0100

    test

--- main/schedulemanager/static/tailwind.config.js
+++ main/schedulemanager/static/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['../templates/**/*.html'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'col-1': '#34495E',
+        'col-2':'#20c997',
+        'col-3':'#007bff'
+      }
+    },
   },
   plugins: [],
 }
