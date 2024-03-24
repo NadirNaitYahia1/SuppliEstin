32fbae98fccef7e5576832b755334163de3efc69
tarekichalalen2002 <t_ichalalen@estin.dz>
2024-03-24 15:04:30 +0100

    installing tailwind-django

--- main/main/urls.py
+++ main/main/urls.py
@@ -21,4 +21,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('schedulemanager.urls')),
+    path('theme/', include('theme.urls')),
 ]
